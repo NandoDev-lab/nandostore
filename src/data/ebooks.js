126825dc9efae18f3684dbd59f99daf59274774e
FERNANDO LUIS DE OLIVEIRA SALDANHA <fernandogspl@hotmail.com>
2026-09-03 03:31:24 -0300

    Adiciona capa do ebook

--- src/data/ebooks.js
+++ src/data/ebooks.js
@@ -6,6 +6,6 @@ export const ebooks = [
     description: 'Um chamado à vigilância para reconhecer os gatilhos que podem conduzir ao pecado e evitar a queda.',
     synopsis: 'Um chamado à vigilância para reconhecer os gatilhos que podem conduzir ao pecado e evitar a queda.',
     category: '',
-    cover: '', pages: '', format: '', price: '', isbn: '', publisher: '', purchaseLink: '', pdf: '', sample: '', featured: true,
+    cover: './images/ebooks/ebook_gatilhos_do_pecado.jpeg', pages: '', format: '', price: '', isbn: '', publisher: '', purchaseLink: '', pdf: '', sample: '', featured: true,
   },
 ]
